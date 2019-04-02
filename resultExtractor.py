d216dd54fe860345deb1f0955de17c42928148f7
Angel <angel_11agustin@hotmail.com>
2019-04-01 21:20:10 -0700

    Update ID fields per smash.gg API update

--- resultExtractor.py
+++ resultExtractor.py
@@ -94,7 +94,7 @@ class Participant:
 
 
 class TournamentSetsRequest:
-    auth_token = '026d66d8eeb4f1e73aa2ebe750388536'
+    auth_token = 'YOUR TOKEN HERE'
     events = []             # {event_id, tournament}
     participants = []       # Id, gamerTag, {tournament, placing}
     participants_dict = {}
